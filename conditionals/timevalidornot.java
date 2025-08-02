02328b4dd42d03b737ac2bcc9309148d86db289f
aadhi-11x <aadharishini.11@gmail.com>
2025-08-02 14:09:16 +0530

    Initial Commit

---
+++ conditionals/timevalidornot.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        String a = sc.next();        
+        String[] tt = a.split(":");
+        int h = Integer.parseInt(tt[0]);
+        int m = Integer.parseInt(tt[1]);
+        int s = Integer.parseInt(tt[2]);
+        if((h >= 0 && h <= 23) && (m >= 0 && m <= 60) && (s >= 0 && s <= 60)){
+            System.out.println("Valid");
+        }
+        else {
+            System.out.println("Not Valid");
+        }
+    }
+}
